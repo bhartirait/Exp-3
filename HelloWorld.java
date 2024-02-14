78c2b2ea2aadfa074a20631f9b499a5960d4164b
bhartirait <156160397+bhartirait@users.noreply.github.com>
2024-02-14 16:24:03 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Worldddddd!"); 
+        System.out.println("Hello, Worldddddd this is RAIT!"); 
     }
 }
